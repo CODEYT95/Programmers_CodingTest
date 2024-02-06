5bbb0a6bacb24742b115d3eac55992ca43804037
codeyt95 <codeyt95@gmail.com>
2024-02-06 22:57:26 +0900

    Solved

--- src/LV_0/DAY1-2.java
+++ src/LV_0/DAY1-2.java
@@ -4,14 +4,18 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+        
         int a = sc.nextInt();
         int b = sc.nextInt();
 
         if(-100000 <= a && a <= 100000){ //a의 제한사항
             if(-100000 <= b && b <= 100000){ //b의 제한사항
-                System.out.println("a = " + a);
+
+              System.out.println("a = " + a);
 	            System.out.println("b = " + b);
+
             }
         }
         
