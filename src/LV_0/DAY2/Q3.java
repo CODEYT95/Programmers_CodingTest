465ba1f7ae6062f7f51b806ec58ed4357326fb40
codeyt95 <codeyt95@gmail.com>
2024-02-08 18:08:38 +0900

    solved

---
+++ src/LV_0/DAY2/Q3.java
@@ -0,0 +1,15 @@
+//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181945
+
+package LV_0.DAY2;
+
+import java.util.Scanner;
+
+public class Solution {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String a = sc.next();
+
+    for (char i : a.toCharArray())
+      System.out.println(i);
+  }
+}
