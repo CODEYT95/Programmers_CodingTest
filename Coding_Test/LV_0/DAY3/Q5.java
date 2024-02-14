3794f789ca62f2589596bb2bb2ef5ee21769cdb9
codeyt95 <codeyt95@gmail.com>
2024-02-14 16:04:32 +0900

    solved

--- Coding_Test/LV_0/DAY3/Q5.java
+++ Coding_Test/LV_0/DAY3/Q5.java
@@ -1,16 +1,21 @@
-//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181940
+//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181938
 
 package LV_0.DAY3;
 
 class Solution {
-  public String solution(String my_string, int k) {
-      String answer = "";
+  public int solution(int a, int b) {
+      int answer = 0;
+      int val = 2 * a * b;
       
-      int length = my_string.length(); //my_string의 문자열 길이 length에 저장
+      String str1 = Integer.toString(a) + Integer.toString(b);
       
-      if(1 <= length && length <= 100 && 1 <= k && k <= 100){ //1 ≤ my_string의 길이 ≤ 100, 1 ≤ k ≤ 100
-          answer = my_string.repeat(k); //repeat(k) 문자열을 k 만큼 반복하는 메소드 
+      if(val < Integer.parseInt(str1)){
+          answer = Integer.parseInt(str1);
+      }else if(Integer.parseInt(str1) < val){
+          answer = val;
+      }else {
+          answer = Integer.parseInt(str1);
       }
-      return answer;//answer 값 리턴하고 새 값 할당
+      return answer;
   }
 }
