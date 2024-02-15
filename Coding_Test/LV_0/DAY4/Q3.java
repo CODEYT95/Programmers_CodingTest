0fa96750d8912e4a44a71a7c27513440f73c9847
codeyt95 <codeyt95@gmail.com>
2024-02-15 13:04:10 +0900

    solved

--- Coding_Test/LV_0/DAY4/Q3.java
+++ Coding_Test/LV_0/DAY4/Q3.java
@@ -1,22 +1,33 @@
-//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181942
+//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181935?language=java
 
-package LV_0.DAY3;
+package LV_0.DAY4;
 
 class Solution {
-  public String solution(String str1, String str2) {
-      String answer = "";
-      
-      int length = str1.length();
-      
-      char [] arr1 = str1.toCharArray(); // str1을 char타입 배열로 값을 저장
-      char [] arr2 = str2.toCharArray(); // str1을 char타입 배열로 값을 저장
-      
-       if(1 <= length && length <= 10){//제한사항
-           for(int i=0; i<length; i++){//for문
-               answer += Character.toString(arr1[i]) + Character.toString(arr2[i]);
-               //answer가 String타입이므로 arr1,2를 타입 변환 해주고 answer에 값 넣어주기
-           }
-       }       
-      return answer;
-  }
+    public int solution(int n) {
+        int answer = 0;
+
+        if (n % 2 != 0) {// n이 홀수일 때
+
+            for (int i = 1; i <= n; i++) {
+
+                if (i % 2 != 0) {
+                    answer += i;
+                }
+
+            }
+
+        } else {// n이 짝수일 때
+
+            for (int i = 1; i <= n; i++) {
+
+                if (i % 2 == 0) {
+                    answer += i * i;
+                }
+
+            }
+
+        }
+
+        return answer;
+    }
 }
