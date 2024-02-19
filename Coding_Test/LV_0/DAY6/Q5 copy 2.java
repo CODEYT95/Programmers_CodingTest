de23aad4a0f579f0a1b06683612ecbde9840f43a
codeyt95 <codeyt95@gmail.com>
2024-02-19 11:52:58 +0900

    solved

--- Coding_Test/LV_0/DAY6/Q5 copy 2.java
+++ Coding_Test/LV_0/DAY6/Q5 copy 2.java
@@ -1,29 +1,31 @@
-//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181928
+//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181926
 
 package LV_0.DAY5;
 
 class Solution {
-    public int solution(int[] num_list) {
+    public int solution(int n, String control) {
         int answer = 0;
-        String odd = "";
-        String even = "";
 
-        if (2 <= num_list.length && num_list.length <= 10) {// 길이 제한사항
+        if (-100000 <= n && n <= 100000 && 1 <= control.length() && control.length() <= 100000) { // 길이 제한사항
 
-            for (int idx : num_list)// for-each문
+            for (char idx : control.toCharArray()) { // for-each문 char타입으로 control에 들어있는 문자 하나씩 출력
 
-                if (1 <= idx && idx <= 9) {// 원소 범위 제한사항
+                if (idx == 'w') { // 문자가 w인경우
+                    n += 1; // n에 +1 더해주기
 
-                    if (idx % 2 == 1) { // 홀수일 때
-                        odd += Integer.toString(idx);
+                } else if (idx == 's') { // 문자가 s인경우
+                    n += -1;// n에 -1 빼주기
 
-                    } else {// 짝수일 때
-                        even += Integer.toString(idx);
-                    }
+                } else if (idx == 'd') { // 문자가 d인경우
+                    n += 10;// n에 +10 더해주기
+
+                } else { // 문자가 a인경우
+                    n += -10;// n에 -10 빼주기
                 }
-            answer = Integer.parseInt(odd) + Integer.parseInt(even); // String타입을 int타입으로 변환 후 값 더 해서 출력
+            }
+            answer = n; // answer에 n의 값 넣기
         }
 
-        return answer;
+        return answer;// 값 반환
     }
 }
