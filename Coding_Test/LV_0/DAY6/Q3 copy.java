fad2e7285cdcb4de31f09207d23549e880c06857
codeyt95 <codeyt95@gmail.com>
2024-02-22 18:56:28 +0900

    solved

--- Coding_Test/LV_0/DAY6/Q3 copy.java
+++ Coding_Test/LV_0/DAY6/Q3 copy.java
@@ -1,32 +1,33 @@
-//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181928
-
+//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181923
 package LV_0.DAY6;
 
 class Solution {
-    public String solution(int[] numLog) {
-        String answer = "";
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        answer = new int[queries.length];
 
-        for (int idx = 1; idx < numLog.length; idx++) {
+        for (int idx = 0; idx < queries.length; idx++) { // queries의 값들을 차례대로 s,e,k에 저장
+            int s = queries[idx][0];
+            int e = queries[idx][1];
+            int k = queries[idx][2];
+            int minVal = 1000000;
 
-            if (numLog[idx] - numLog[idx - 1] == 1) { // 1인경우
+            for (int i = s; i <= e; i++) { // s ≤ i ≤ e 로 arr배열의 범위 설정
 
-                answer += "w";
+                if (k < arr[i] && arr[i] < minVal) {// arr[i]가 k보다 크면서 범위 중 가장 작은 값
 
-            } else if (numLog[idx] - numLog[idx - 1] == -1) { // -1인경우
+                    minVal = arr[i];
+                    answer[idx] = minVal;
 
-                answer += "s";
+                }
 
-            } else if (numLog[idx] - numLog[idx - 1] == 10) { // 10인경우
-
-                answer += "d";
-
-            } else if (numLog[idx] - numLog[idx - 1] == -10) { // -10인경우
-
-                answer += "a";
-
+            }
+            if (minVal == 1000000) {
+                answer[idx] = -1;
             }
 
         }
+
         return answer; // 값 반환
     }
 }
