f7c353b795c4753ea83d05b5e793575c04a1801c
codeyt95 <codeyt95@gmail.com>
2024-02-23 19:04:05 +0900

    solved

--- Coding_Test/LV_0/DAY7/Q2.java
+++ Coding_Test/LV_0/DAY7/Q2.java
@@ -1,28 +1,39 @@
-//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181927
+//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181921
 
-package LV_0.DAY6;
+package LV_0.DAY7;
+
+import java.util.LinkedList;
 
 class Solution {
-    public int[] solution(int[] num_list) {
+    public int[] solution(int l, int r) {
         int[] answer = {};
-        if (2 <= num_list.length && num_list.length <= 10) { // 길이 제한사항
 
-            answer = new int[num_list.length + 1]; // num_list 보다 길이가 1 더 긴 int answer 배열 만들기
+        LinkedList<Integer> link = new LinkedList<Integer>();// LinkedList 객체 생성
 
-            for (int idx = 0; idx < num_list.length; idx++) {// for문 이용해서 answer배열에 num_list 배열 옮기기
+        for (int i = l; i <= r; i++) {// l이상r이하 for문 생성
 
-                answer[idx] = num_list[idx];
-            }
-            if (answer[answer.length - 2] > answer[answer.length - 3]) {// 마지막 원소가 그전 원소보다 큰 경우
+            if (Integer.toString(i).matches("^[05]+$")) { // 0과 5로만 이루어 졌을 때
 
-                answer[answer.length - 1] = answer[answer.length - 2] - answer[answer.length - 3];
+                link.add(i); // 배열에 i의 값 추가
 
-            } else {// 마지막 원소가 그전 원소보다 작은 경우
-
-                answer[answer.length - 1] = answer[answer.length - 2] * 2;
             }
         }
 
-        return answer; // 값 반환
+        if (link.isEmpty()) { // 배열이 비었을 경우
+
+            answer = new int[1];
+
+            answer[0] = -1; // 배열이 비었을 때 -1 출력
+
+        } else {// 배열에 값이 있을 경우
+
+            answer = new int[link.size()];
+
+            for (int k = 0; k < link.size(); k++) { // answer로 값 옮기기
+                answer[k] = link.get(k);
+            }
+        }
+
+        return answer;// 값 반환
     }
 }
