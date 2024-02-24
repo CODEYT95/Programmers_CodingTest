333bc6ed60c53a8e1a1a0d72780947c731fffa04
codeyt95 <codeyt95@gmail.com>
2024-02-24 23:26:08 +0900

    solved

--- Coding_Test/LV_0/DAY8/Q1 copy 2.java
+++ Coding_Test/LV_0/DAY8/Q1 copy 2.java
@@ -1,17 +1,15 @@
-//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181917
+//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181915
 
 package LV_0.DAY8;
 
 class Solution {
-    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
-        boolean answer = true;
+    public String solution(String my_string, int[] index_list) {
+        String answer = "";
 
-        if ((x1 || x2) && (x3 || x4) == true) {// ∨는 하나가 true면 true
-                                               // ∧는 둘 다 true여야지 true
+        for (int i : index_list - 1) { // index_list길이 만큼의 for문
 
-        } else {
-
-            answer = false;
+            answer += my_string.charAt(index_list[i]);// index_list에 들어있는 숫자를 차례대로 뽑아 그 숫자에 해당하는 my_string의 문자를 answer에
+                                                      // 넣기
 
         }
 
