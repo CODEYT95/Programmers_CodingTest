b4e770376a5310f9fc594cfb2b5384c592f11f77
codeyt95 <codeyt95@gmail.com>
2024-02-24 23:23:47 +0900

    solved

--- Coding_Test/LV_0/DAY8/Q1.java
+++ Coding_Test/LV_0/DAY8/Q1.java
@@ -1,33 +1,20 @@
-//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181922
+//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181917
 
-package LV_0.DAY7;
+package LV_0.DAY8;
 
 class Solution {
-    public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = {};
-        answer = new int[arr.length];
+    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        boolean answer = true;
 
-        for (int idx = 0; idx < queries.length; idx++) { // queries의 값들을 차례대로 s,e,k에 저장
+        if ((x1 || x2) && (x3 || x4) == true) {// ∨는 하나가 true면 true
+                                               // ∧는 둘 다 true여야지 true
 
-            int s = queries[idx][0];
-            int e = queries[idx][1];
-            int k = queries[idx][2];
+        } else {
 
-            for (int i = s; i <= e; i++) { // s ≤ i ≤ e 로 arr배열의 범위 설정
+            answer = false;
 
-                if (i == 0 || i % k == 0) { // i가 0인 경우 or i가 k의 배수인 경우
-
-                    arr[i] += 1;
-
-                }
-            }
-        }
-
-        for (int i = 0; i < arr.length; i++) {// answer에 값 옮겨주기
-            answer[i] = arr[i];
         }
 
         return answer;// 값 반환
     }
-
 }
