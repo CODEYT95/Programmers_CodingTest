cf2890c1ce09d2b953e0a3335eae3cf334ce2cfb
codeyt95 <codeyt95@gmail.com>
2024-02-25 18:03:45 +0900

    solved

--- Coding_Test/LV_0/DAY8/Q3 copy 2.java
+++ Coding_Test/LV_0/DAY8/Q3 copy 2.java
@@ -1,18 +1,16 @@
-//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181915
+//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181914
 
 package LV_0.DAY8;
 
 class Solution {
-    public String solution(String my_string, int[] index_list) {
-        String answer = "";
+    public int solution(String number) {
+        int answer = 0;
 
-        for (int i : index_list - 1) { // index_list길이 만큼의 for문
+        for (int i = 0; i < number.length(); i++) {
 
-            answer += my_string.charAt(index_list[i]);// index_list에 들어있는 숫자를 차례대로 뽑아 그 숫자에 해당하는 my_string의 문자를 answer에
-                                                      // 넣기
+            answer += Integer.parseInt(String.valueOf(number.charAt(i)));
 
         }
-
-        return answer;// 값 반환
+        return answer % 9;
     }
 }
