5d744f5276ba8aec8fc97e4122af4e0cbefd72f9
codeyt95 <codeyt95@gmail.com>
2024-02-25 18:04:33 +0900

    solved

--- Coding_Test/LV_0/DAY8/Q3 copy.java
+++ Coding_Test/LV_0/DAY8/Q3 copy.java
@@ -1,18 +1,40 @@
-//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181915
-
+//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181913
 package LV_0.DAY8;
 
 class Solution {
-    public String solution(String my_string, int[] index_list) {
+    public String solution(String my_string, int[][] queries) {
         String answer = "";
+        char temp1; // 임시로 값 옮겨둘 곳1
+        char temp2; // 임시로 값 옮겨둘 곳2
+        char arr[] = my_string.toCharArray(); // my_string 배열로 만들기
 
-        for (int i : index_list - 1) { // index_list길이 만큼의 for문
+        for (int i = 0; i < queries.length; i++) {// s,e값 for문으로 만들기
 
-            answer += my_string.charAt(index_list[i]);// index_list에 들어있는 숫자를 차례대로 뽑아 그 숫자에 해당하는 my_string의 문자를 answer에
-                                                      // 넣기
+            int s = queries[i][0];
+            int e = queries[i][1];
 
+            for (int k = s; s < e; k++) {
+
+                if (k < e) {// e가k보다 클 때
+
+                    // 임시로 값 저장
+                    temp1 = arr[k];
+                    temp2 = arr[e];
+
+                    // 값 서로 바꿔주기
+                    arr[k] = temp2;
+                    arr[e] = temp1;
+
+                }
+
+                e--;
+            }
+        }
+        for (char i : arr) {// 값 옮겨주기
+
+            answer += i;
         }
 
-        return answer;// 값 반환
+        return answer;
     }
 }
