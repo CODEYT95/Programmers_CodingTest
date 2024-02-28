1a8a0fddd08c65743fbfb24801bdd6a0bd449645
codeyt95 <codeyt95@gmail.com>
2024-02-28 20:22:17 +0900

    solved

--- Coding_Test/LV_0/DAY10/Q5.java
+++ Coding_Test/LV_0/DAY10/Q5.java
@@ -1,18 +1,30 @@
-//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181908
+//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181905
 
-package LV_0.DAY9;
+package LV_0.DAY10;
 
 class Solution {
-    public int solution(String my_string, String is_suffix) {
-        int answer = 0;
+    public String solution(String my_string, int s, int e) {
+        String answer = "";
+        char temp1;
+        char temp2;
+        char arr[] = my_string.toCharArray(); // char타입 arr 배열에 my_string문자열 넣어주기
 
-        for (int i = 0; i < my_string.length(); i++) {
+        for (int i = s; i < e; i++) {// 값 바꿔주기
 
-            if (my_string.substring(my_string.length() - i - 1).equals(is_suffix)) {// .equals메소드로 문자열 비교
+            if (i < e) {
+                temp1 = arr[i];
+                temp2 = arr[e];
 
-                answer = 1; // 접미사가 맞으면 answer = 1
+                arr[i] = temp2;
+                arr[e] = temp1;
 
+                e--;
             }
+        }
+
+        for (int i = 0; i < my_string.length(); i++) {// answer에 값 옮겨주기
+
+            answer += arr[i];
 
         }
 
