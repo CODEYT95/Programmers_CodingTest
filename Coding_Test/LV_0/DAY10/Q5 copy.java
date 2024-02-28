fb4cee0089e73c62727a8bba3dfc155b03af82ec
codeyt95 <codeyt95@gmail.com>
2024-02-28 20:21:30 +0900

    solved

--- Coding_Test/LV_0/DAY10/Q5 copy.java
+++ Coding_Test/LV_0/DAY10/Q5 copy.java
@@ -1,16 +1,16 @@
-//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181908
+//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181906
 
-package LV_0.DAY9;
+package LV_0.DAY10;
 
 class Solution {
-    public int solution(String my_string, String is_suffix) {
+    public int solution(String my_string, String is_prefix) {
         int answer = 0;
 
-        for (int i = 0; i < my_string.length(); i++) {
+        for (int i = 0; i < my_string.length(); i++) {// my_string을 이용해 for문 생성
 
-            if (my_string.substring(my_string.length() - i - 1).equals(is_suffix)) {// .equals메소드로 문자열 비교
+            if (my_string.substring(0, i + 1).equals(is_prefix)) {// .equals메소드로 문자열 비교
 
-                answer = 1; // 접미사가 맞으면 answer = 1
+                answer = 1; // 접두사가 맞으면 answer = 1
 
             }
 
