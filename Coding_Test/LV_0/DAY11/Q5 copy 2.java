25a12059bcf369a8f4ec412c9884cfef4e27a9ff
codeyt95 <codeyt95@gmail.com>
2024-03-01 23:42:07 +0900

    solved

--- Coding_Test/LV_0/DAY11/Q5 copy 2.java
+++ Coding_Test/LV_0/DAY11/Q5 copy 2.java
@@ -1,22 +1,28 @@
-//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181903
+//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181902
 
-package LV_0.DAY10;
+package LV_0.DAY11;
 
 class Solution {
-    public String solution(int q, int r, String code) {
-        String answer = "";
-        char arr[] = code.toCharArray();// char타입 배열에 code넣기
+    public int[] solution(String my_string) {
+        int[] answer = {};
+        char arr[] = my_string.toCharArray();// my_string을 char타입 배열로 만들기
+        answer = new int[52];
 
-        for (int idx = 0; idx < code.length(); idx++) {// code길이를 이용한 for문 생성
+        for (int i = 0; i < my_string.length(); i++) {// my_string을 이용한 for문 생성
 
-            if (idx % q == r) { // 인덱스를 q로 나눈 나머지가 r인 경우
+            int ascii = (int) arr[i]; // my_string에 담긴 문자를 숫자(아스키코드)로 만들기
 
-                answer += arr[idx]; // answer에 값 넣기
+            if (65 <= ascii && ascii <= 90) {// 대문자인 경우
 
+                answer[ascii - 65] += 1;
+
+            } else if (97 <= ascii && ascii <= 122) {// 소문자인 경우
+
+                answer[ascii - 71] += 1;
             }
 
         }
 
-        return answer;// 값 반환
+        return answer; // 값 반환
     }
 }
