167f9e09eca68e2570e84eb612ce5c2fc1225c96
codeyt95 <codeyt95@gmail.com>
2024-03-03 21:12:03 +0900

    solved

--- Coding_Test/LV_0/DAY11/Q3 copy.java
+++ Coding_Test/LV_0/DAY11/Q3 copy.java
@@ -1,24 +1,17 @@
-//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181900
+//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181898
 
 package LV_0.DAY11;
 
 class Solution {
-    public String solution(String my_string, int[] indices) {
-        String answer = "";
-        char arr[] = my_string.toCharArray(); // my_string char타입 배열로 만들어주기
+    public int solution(int[] arr, int idx) {
+        int answer = -1;
 
-        for (int i = 0; i < indices.length; i++) {// indices길이를 이용한 for문 생성
+        for (int i = 0; i < arr.length; i++) { // arr의 길이를 이용한 for문 생성
 
-            arr[indices[i]] = '0';// indices[i]에 해당하는 arr값 '0' 으로 바꿔주기
+            if (i >= idx && arr[i] == 1) { // i가 idx보다 크거나 같으면서 1인 가장 작은 인덱스
 
-        }
-
-        for (char i : arr) {// arr배열의 값 for-each로 뽑기
-
-            if (i != '0') {// i 가 문자'0'이 아닌 경우
-
-                answer += i;// answer에 값 넣어주기
-
+                answer = i;// 가장 작은 인덱스를 찾아야 하므로 1을 찾으면 break를 써서 for문 종료
+                break;
             }
 
         }
