a9bb9fc653673536e66287674296a3435cf602eb
codeyt95 <codeyt95@gmail.com>
2024-03-04 21:58:49 +0900

    solved

--- Coding_Test/LV_0/DAY12/Q5 copy.java
+++ Coding_Test/LV_0/DAY12/Q5 copy.java
@@ -1,17 +1,19 @@
 //문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181898
 
-package LV_0.DAY11;
+package LV_0.DAY12;
 
 class Solution {
-    public int solution(int[] arr, int idx) {
-        int answer = -1;
+    public int solution(int[] num_list) {
+        int answer = -1; // 음수가 없으면 -1 그대로 출력
 
-        for (int i = 0; i < arr.length; i++) { // arr의 길이를 이용한 for문 생성
+        for (int i = 0; i < num_list.length; i++) {// num_list길이를 이용한 for문 생성
 
-            if (i >= idx && arr[i] == 1) { // i가 idx보다 크거나 같으면서 1인 가장 작은 인덱스
+            if (num_list[i] < 0) {// num_list[i] 값이 음수일 때
 
-                answer = i;// 가장 작은 인덱스를 찾아야 하므로 1을 찾으면 break를 써서 for문 종료
-                break;
+                answer = i; // answer에 해당 인덱스 넣기
+
+                break;// 음수일 경우를 찾으면 for문 바로 종료
+
             }
 
         }
