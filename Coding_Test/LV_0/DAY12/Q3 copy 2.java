6de05297145d5f948fb3df63c596d141a4a6ebfe
codeyt95 <codeyt95@gmail.com>
2024-03-13 02:17:16 +0900

    solved

---
+++ Coding_Test/LV_0/DAY12/Q3 copy 2.java
@@ -0,0 +1,25 @@
+//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181895
+
+package LV_0.DAY12;
+
+import java.util.LinkedList;
+
+class Solution {
+    public LinkedList<Integer> solution(int[] arr, int[][] intervals) {
+        LinkedList<Integer> answer = new LinkedList<Integer>();
+
+        for (int i = 0; i < intervals.length; i++) {// intervals길이를 이용한 for문 생성
+
+            int s = intervals[i][0]; // 시작 값
+            int e = intervals[i][1]; // 끝 값
+
+            for (int j = s; j <= e; j++) {// 양끝값의 사이의 값을 이용한 for문 생성
+
+                answer.addLast(arr[j]);// answer에 값 넣어주기
+
+            }
+        }
+
+        return answer;
+    }
+}
