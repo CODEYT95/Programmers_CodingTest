ccedf7faa524f0ce9de5fc4044ca26b64e6829fa
codeyt95 <codeyt95@gmail.com>
2024-03-17 19:18:48 +0900

    solved

--- Coding_Test/LV_0/DAY13/Q5.java
+++ Coding_Test/LV_0/DAY13/Q5.java
@@ -2,31 +2,40 @@
 
 package LV_0.DAY12;
 
-import java.util.LinkedList;
+import java.util.*;
 
 class Solution {
-    public LinkedList<Integer> solution(int[] arr, int[] query) {
-        LinkedList<Integer> answer = new LinkedList<Integer>();
-
-        for (int i : arr) {
-            answer.addLast(i);
-        } // answer에 arr값 옮겨담기
-
-        for (int i = 0; i < query.length; i++) {// query길이를 이용한 for문 생성
-
-            if (i % 2 == 0) {// i가 짝수일 때
-
-                answer.subList(query[i] + 1, answer.size()).clear();
-                // query[i]값의 인덱스 제외 뒷부분 다 자르기
-
-            } else if (i % 2 == 1) {// i가 홀수일 때
-
-                answer.subList(0, query[i]).clear();//
-                // query[i]값의 인덱스 제외 앞부분 다 자르기
+    public LinkedList<String> solution(String[] str_list) {
+        LinkedList<String> answer = new LinkedList<String>();
+        String str = "";
+        for(String i : str_list){str += i;}
+        
+        if(str.contains("l") || str.contains("r")){//l만 있거나 / r만 있거나  / 둘 다 있거나
+            
+            for(int i=0; i<str_list.length; i++){//str_list길이를 이용한 for문
+            
+                if(str_list[i].equals("l")){//l이 먼저 나올 때
+                
+                    for(int j=0; j<i; j++){
+                    
+                        answer.add(str_list[j]);//answer에 값 넣어주기
+                
+                    }   
+                    break;
+                }else if(str_list[i].equals("r")){//r이 먼저 나올 때
+                
+                    for(int j=i+1; j<str_list.length; j++){
+                   
+                        answer.add(str_list[j]);//answer에 값 넣어주기
+                
+                    }
+                    break;
+                }  
+            
             }
 
         }
-
-        return answer;// 값 반환
+        
+        return answer;//값 반환
     }
 }
