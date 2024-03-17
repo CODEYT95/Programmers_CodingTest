8498b5a1c2ae562e79abb72774550f88903599fa
codeyt95 <codeyt95@gmail.com>
2024-03-17 19:18:27 +0900

    solved

--- Coding_Test/LV_0/DAY13/Q1.java
+++ Coding_Test/LV_0/DAY13/Q1.java
@@ -4,13 +4,13 @@ package LV_0.DAY13;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        
-        int answer[] = new int[num_list.length-(n-1)];//answer길이 정해주기
-                
-        for(int i=0; i<answer.length; i++){
-            answer[i] = num_list[(n-1)+i];//answer에 값 넣어주기
+
+        int answer[] = new int[num_list.length - (n - 1)];// answer길이 정해주기
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = num_list[(n - 1) + i];// answer에 값 넣어주기
         }
-        
-        return answer;//값 반환
+
+        return answer;// 값 반환
     }
 }
