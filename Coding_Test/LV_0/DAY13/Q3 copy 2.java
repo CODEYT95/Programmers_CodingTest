5f314dd6c87b617232f7dd893d6ecb2ab2095678
codeyt95 <codeyt95@gmail.com>
2024-03-18 23:21:18 +0900

    solved

--- Coding_Test/LV_0/DAY13/Q3 copy 2.java
+++ Coding_Test/LV_0/DAY13/Q3 copy 2.java
@@ -2,22 +2,11 @@
 
 package LV_0.DAY13;
 
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer = {};
-        int count = 0;
 
-        for (int i = 0; i < num_list.length; i += n) {// num_list길이를 이용한 for문 생성
-
-            count++;// answer길이 count에 저장
-        }
-
-        answer = new int[count];// answer 길이 생성
-        for (int i = 0; i < answer.length; i++) {//// answer길이를 이용한 for문 생성
-
-            answer[i] = num_list[n * i]; // answer에 값 넣어주기
-
-        }
-        return answer;// 값 반환
+        return Arrays.copyOfRange(num_list, 0, n); // Arrays.copyOfRange()로 n번째 원소까지~
     }
 }
