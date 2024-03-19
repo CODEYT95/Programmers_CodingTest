bf9bc2c68ca4f0ecb27c6a4e038be3eca598e5d6
codeyt95 <codeyt95@gmail.com>
2024-03-19 23:19:10 +0900

    solved

--- Coding_Test/LV_0/DAY14/Q5 copy 2.java
+++ Coding_Test/LV_0/DAY14/Q5 copy 2.java
@@ -3,21 +3,30 @@
 package LV_0.DAY13;
 
 class Solution {
-    public int[] solution(int[] num_list, int n) {
-        int[] answer = {};
-        int count = 0;
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int odd = 0;// 홀
+        int even = 0;// 짝
 
-        for (int i = 0; i < num_list.length; i += n) {// num_list길이를 이용한 for문 생성
+        for (int i = 0; i < num_list.length; i++) {// num_list길이를 이용한 for문
 
-            count++;// answer길이 count에 저장
+            if ((i + 1) % 2 == 0) {// 홀수
+
+                odd += num_list[i];
+
+            } else {// 짝수
+
+                even += num_list[i];
+
+            }
+
+        }
+        if (odd > even) {// 홀수가 클 때
+            answer = odd;
+        } else { // 짝수가 크거나 같을 때
+            answer = even;
         }
 
-        answer = new int[count];// answer 길이 생성
-        for (int i = 0; i < answer.length; i++) {//// answer길이를 이용한 for문 생성
-
-            answer[i] = num_list[n * i]; // answer에 값 넣어주기
-
-        }
         return answer;// 값 반환
     }
 }
