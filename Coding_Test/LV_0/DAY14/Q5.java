900a92a56a3b134784df9f46c635f5dbc9052861
codeyt95 <codeyt95@gmail.com>
2024-03-19 23:21:02 +0900

    solved

--- Coding_Test/LV_0/DAY14/Q5.java
+++ Coding_Test/LV_0/DAY14/Q5.java
@@ -2,22 +2,21 @@
 
 package LV_0.DAY13;
 
+import java.util.*;
+
 class Solution {
-    public int[] solution(int[] num_list, int n) {
-        int[] answer = {};
-        int count = 0;
+    public List<String> solution(String[] todo_list, boolean[] finished) {
 
-        for (int i = 0; i < num_list.length; i += n) {// num_list길이를 이용한 for문 생성
+        List<String> answer = new ArrayList<String>();// ArrayList 객체 생성
 
-            count++;// answer길이 count에 저장
+        for (int i = 0; i < finished.length; i++) {// finished길이를 이용한 for문 생성
+
+            if (finished[i] == false) {// false일 때
+
+                answer.add(todo_list[i]);// answer에 값 넣어주기
+            }
         }
 
-        answer = new int[count];// answer 길이 생성
-        for (int i = 0; i < answer.length; i++) {//// answer길이를 이용한 for문 생성
-
-            answer[i] = num_list[n * i]; // answer에 값 넣어주기
-
-        }
         return answer;// 값 반환
     }
 }
